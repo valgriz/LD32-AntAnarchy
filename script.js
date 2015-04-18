aa08186671a937184c6b1fdf01ddf304a583072b
Joshua Downes <JoshuaDownes@github.com>
2015-04-18 14:43:46 -0400

    fixed generation issue

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var sprite;
 var cursors;
 var ground;
 var ant;
+var platforms;
 
 //controls
 var leftKey;
@@ -17,15 +18,18 @@ var aKey;
 var dKey;
 var wKey;
 
+
 function preload(){
-	game.load.image('backgrounda1','assets/images/backgrounda1.png');
+	game.load.image('backgroundA1','assets/images/backgroundA1.png');
 	game.load.image('grounda1', 'assets/images/grounda3.png');
 	game.load.spritesheet('ant', 'assets/images/anta5.png', 44, 24, 5);
+	game.load.image('platforma1', 'assets/images/platforma1.png');
 }
+
 
 function create(){
 
-	game.add.image(0, 0, 'backgrounda1');
+	game.add.image(0, 0, 'backgroundA1');
 
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -35,13 +39,21 @@ function create(){
 	ant = game.add.sprite(50, 350, 'ant');
 	ant.animations.add('walk', [0,1,2,3,4], 15, true);
 
-
 	game.physics.enable([ant, ground], Phaser.Physics.ARCADE);
+	
+	//Loads first level
+	platforms = new Phaser.Group(game);
+	loadLevel1();
 
 	ant.body.gravity.y = 400;
 	ant.anchor.setTo(.5, .5);
 
+	//Enables physics for all loaded platforms
 	ground.body.immovable = true;
+	platforms.forEach(function(item){
+	    game.physics.enable([ant, item], Phaser.Physics.ARCADE);
+	    item.body.immovable = true;
+	}, this);
 
 	//Defines input keys
 	leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
@@ -55,7 +67,10 @@ function create(){
 
 function update(){
  	game.physics.arcade.collide(ant, ground);
-
+	
+	platforms.forEach(function(item){
+	    game.physics.arcade.collide(ant,item);
+	}, this);
 
  	if(leftKey.isDown || aKey.isDown){
  		ant.body.velocity.x = -270;
@@ -76,3 +91,16 @@ function update(){
  	//Handles input
 
 }
+
+
+function loadLevel1(){
+	var read = new XMLHttpRequest();
+	read.open('GET', 'assets/levels/level1.txt', false);
+	read.send();
+	var platformInfo = read.responseText.split(',');
+	for(i = 0; i < platformInfo.length; i+=2){
+		platforms.add(game.add.sprite(parseInt(platformInfo[i]), parseInt(platformInfo[i+1]), 'platforma1'));	
+	}
+}
+
+
