e7041a52bdd0059e4606d9e1124067cc7226a9d7
Steven Marturano <steven.marturano@gmail.com>
2015-04-18 18:55:13 -0400

    pillar method

--- script.js
+++ script.js
@@ -24,6 +24,9 @@ var wKey;
 var landing;
 var onGround;
 
+var pillar;
+var pillars;
+
 var lTimer;
 
 function preload(){
@@ -31,6 +34,15 @@ function preload(){
 	game.load.image('grounda1', 'assets/images/grounda3.png');
 	game.load.spritesheet('ant', 'assets/images/anta8.png', 44, 24, 21);
 	game.load.image('platforma1', 'assets/images/platforma1.png');
+
+	game.load.image('pi1', 'assets/images/pi_1.png');
+	game.load.image('pi2', 'assets/images/pi_2.png');
+	game.load.image('pi3', 'assets/images/pi_3.png');
+	game.load.image('pi4', 'assets/images/pi_4.png');
+	game.load.image('pi5', 'assets/images/pi_5.png');
+	game.load.image('pi6', 'assets/images/pi_6.png');
+	game.load.image('pi7', 'assets/images/pi_7.png');
+
 }
 
 
@@ -39,7 +51,7 @@ function create(){
 
 	onGround = false;
 
-    game.world.setBounds(0, 0, 1920, 560);
+    game.world.setBounds(0, 0, 3000, 560);
 	game.add.image(0, 0, 'backgroundA1');
 
 	game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -49,12 +61,19 @@ function create(){
 	//Loads first level
 	platforms = new Phaser.Group(game);
 	loadLevel1();
-	levelSize = 800;
-
 
 	ground = game.add.sprite(0, game.world.height - game.cache.getImage('grounda1').height, 'grounda1');
-	ant = game.add.sprite(50, 350, 'ant');
+
+	pillar = game.add.group();
+	pillars = game.add.group();
+
+
+	generatePillar(0,0);
+	generatePillar(700, 4);
 	
+
+	
+	ant = game.add.sprite(170, 350, 'ant');
 
 	ant.animations.add('idle', [0,1,2,3,4,5,6], 10, true);
 	ant.animations.add('run', [7,8,9,10,11,12,13], 10, true);
@@ -69,7 +88,9 @@ function create(){
 	lTimer = 0;
 
 	//Enables physics for all loaded platforms
+
 	ground.body.immovable = true;
+
 	platforms.forEach(function(item){
 	    game.physics.enable([ant, item], Phaser.Physics.ARCADE);
 	    item.body.checkCollision.down = false;
@@ -91,7 +112,43 @@ function create(){
 }
 
 
+
+function generatePillar(xPos, crackPos){
+
+
+	for(var i = 0; i < 14; i++){
+		var pil;
+		if(i == 0){
+			pil = pillar.create(xPos, 40 * i, 'pi7');
+		} else if (i==1){
+			pil = pillar.create(xPos, 40 * i, 'pi6');
+		} else if (i==12){
+			pil = pillar.create(xPos, 40 * i, 'pi2');
+		} else if (i==13){
+			pil = pillar.create(xPos, 40 * i, 'pi1');
+		} else {
+			pil = pillar.create(xPos, 40 * i, 'pi3');
+		}
+		pil.enableBody = true;
+		game.physics.arcade.enable(pil);
+		pil.body.immovable = true;
+	}
+
+	if(crackPos>1 && crackPos<10){
+		crackPos += 2;
+		var temp = pillar.getAt(crackPos);
+
+		temp = pillar.create(xPos, 40*crackPos, 'pi4')
+		var temp2 = pillar.getAt(crackPos+1);
+		temp2 = pillar.create(xPos, 40*(crackPos-1), 'pi5');
+	}
+
+}
+
+
 function update(){
+
+	game.physics.arcade.collide(ant, pillar, pillCollide, null, this);
     
     if(!onGround){
 		platforms.forEach(function(item){
@@ -147,6 +204,10 @@ function update(){
  	}
  	//Handles input
 
+}
+
+function pillCollide(ant, pilC){
+	pilC.kill();
 }
 
 
