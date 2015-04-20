e485e5c6c9ed43f7c152524569a2945779184300
Steven Marturano <steven.marturano@gmail.com>
2015-04-19 21:56:06 -0400

    fixed camera, improved playability

--- script.js
+++ script.js
@@ -21,8 +21,6 @@ var aKey;
 var dKey;
 var wKey;
 var spaceBar;
-
-
 var landing;
 var onGround;
 
@@ -30,7 +28,6 @@ var pillar;
 var pillars;
 var pillarBase;
 var backDrops;
-
 
 var lTimer;
 
@@ -41,18 +38,22 @@ var backMove;
 
 var shouldGenerate;
 var backgroundGenPos;
-
 var cobblestoneGroup;
 
 var nextColumnXPosition;
+var isDead;
+var deadTimer;
+
+var originXPosition;
+var explosion;
 
 function preload(){
 	game.load.image('backgroundA1','assets/images/backgroundA2.png');
 	game.load.image('grounda1', 'assets/images/grounda4.png');
-	game.load.spritesheet('ant', 'assets/images/anta8.png', 44, 24, 21);
+	game.load.spritesheet('ant', 'assets/images/anta12.png', 44, 24, 23);
+	game.load.spritesheet('explosion', 'assets/images/explosiona1.png', 71, 100, 18);
 	game.load.image('platforma1', 'assets/images/platformb1.png');
 	game.load.image('spikes', 'assets/images/spikes.png');
-
 	game.load.image('pi1', 'assets/images/pi_1.png');
 	game.load.image('pi2', 'assets/images/pi_2.png');
 	game.load.image('pi3', 'assets/images/pit_3.png');
@@ -64,17 +65,9 @@ function preload(){
 
 
 function create(){
-
-
 	onGround = false;
 
     game.world.setBounds(0, 0, 1640, 560);
-
-
-	//backTrack = game.add.sprite(0, 0, 'backgroundA1');
-	//backTrack.enableBody = true;
-
-
 	backDrops = game.add.group();
 
 	shouldGenerate = 0;
@@ -89,26 +82,28 @@ function create(){
 	//Loads first level
 	platforms = new Phaser.Group(game);
 	loadLevel1();
-
+	deadTimer = 0;
 
 	cobblestoneGroup = new Phaser.Group(game);
-//>>>>>>>>>>>>>>>>
-	//ground = game.add.sprite(0, game.world.height - game.cache.getImage('grounda1').height, 'grounda1');
-
 	cobblestoneGroup.create(0, game.world.height - game.cache.getImage('grounda1').height, 'grounda1');
 
 	pillar = game.add.group();
 	pillars = game.add.group();
 	pillarBase = game.add.group();
-	
 
-
+	isDead = false;
 
 	generatePillar(460,0);
 	nextColumnXPosition = 1180;
 	generatePillar(nextColumnXPosition, 8);
+
+
+
+	originXPosition = 620;
+
+	ant = game.add.sprite(originXPosition, -50, 'ant');
+
 	
-	ant = game.add.sprite(820, 350, 'ant');
 
 	/////////////////////////////
 	//Spawn spikes
@@ -122,6 +117,12 @@ function create(){
 	ant.animations.add('run', [7,8,9,10,11,12,13], 10, true);
 	ant.animations.add('land', [14,15,16,17,18,19,20], 15, false);
 	ant.animations.add('jump', 21, 10, true);
+	ant.animations.add('dead', [22], 10, true);
+
+
+	explosion = game.add.sprite(900, 100, 'explosion');
+	explosion.animations.add('explode', [0,1,2,3,4,5,6,7,8,9,10,11,12,12,13,14,15,16,17], 20, false);
+	explosion.animations.add('stop', [17], 20, true);
 
 	game.physics.enable([ant, backDrops], Phaser.Physics.ARCADE);
 
@@ -130,10 +131,7 @@ function create(){
 	ant.anchor.setTo(.5, .5);
 	lTimer = 0;
 
-	//Enables physics for all loaded platforms
-
-	//ground.body.immovable = true;
-
+	explosion.animations.play('stop');
 
 	cobblestoneGroup.forEach(function(item){
 			game.physics.enable([ant, item], Phaser.Physics.ARCADE);
@@ -152,6 +150,8 @@ function create(){
 	    sp.body.immovable = true;
 	}, this);
 
+	generateMorePlatforms(10);
+
 	//Defines input keys
 	leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
 	rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
@@ -165,7 +165,16 @@ function create(){
 
 }
 
-
+function explode(xPos, yPos){
+	if(!isDead){
+		explosion.x = xPos - 10;
+		explosion.y = yPos - 75;
+		explosion.scale.x = 2;
+		explosion.scale.y = 2;
+		explosion.play('explode');
+		isDead = true;
+	}
+}
 
 function generatePillar(xPos, crackPos){
 	for(var i = 0; i < 14; i++){
@@ -205,19 +214,22 @@ function generatePillar(xPos, crackPos){
 	}
 }
 
-
 function update(){
-
-	game.physics.arcade.collide(ant, pillar, pillCollide, null, this);
-	game.physics.arcade.collide(ant, pillarBase, pillCollide, null, this);
-
-	//////////////////////////////////////////////
-	// EDIT FOR SPIKE DEATH
-	// ///////////////////////////////////////////
-	spikes.forEach(function(item){
-	    game.physics.arcade.collide(ant, item, function(){theDeadMethod();}, null, this);
-	}, this);
-    
+	if(!isDead){
+		game.physics.arcade.collide(ant, pillar, pillCollide, null, this);
+		game.physics.arcade.collide(ant, pillarBase, pillCollide, null, this);
+		//////////////////////////////////////////////
+		// EDIT FOR SPIKE DEATH
+		// ///////////////////////////////////////////
+		spikes.forEach(function(item){
+		    game.physics.arcade.collide(ant, item, function(){theDeadMethod();}, null, this);
+		}, this);
+    } else {
+    	ant.animations.play('dead');
+    	if(ant.body.velocity > 0){
+    		ant.body.velocity = -ant.body.velocity;
+    	}
+    }
     if(!onGround){
 		platforms.forEach(function(item){
 		    game.physics.arcade.collide(ant, item, onSurface, null, this);
@@ -240,48 +252,74 @@ function update(){
 
  	lTimer++;
 
- 	if(leftKey.isDown || aKey.isDown){
- 		ant.body.velocity.x = -270;
- 		ant.scale.x = -1;
- 		if(lTimer > 30){
- 			ant.animations.play('run');
- 		}
- 		//backTrack.body.velocity.x = 20;
+ 	if(!isDead){
+	 	if(leftKey.isDown || aKey.isDown){
+	 		ant.body.velocity.x = -270;
+	 		ant.scale.x = -1;
+	 		if(lTimer > 30){
+	 			ant.animations.play('run');
+	 		}
+	 		//backTrack.body.velocity.x = 20;
 
- 	} else if(rightKey.isDown || dKey.isDown){
- 		ant.body.velocity.x = 270;
- 		ant.scale.x = 1;
- 		if(lTimer > 30){
- 			ant.animations.play('run');
- 		}
- 	} else {
- 		//ant.body.velocity.x = 0;
- 		if(lTimer > 30){
- 			ant.animations.play('idle');
- 		}
- 		ant.body.acceleration.x = -5 * ant.body.velocity.x;
- 	}
+	 	} else if(rightKey.isDown || dKey.isDown){
+	 		ant.body.velocity.x = 270;
+	 		ant.scale.x = 1;
+	 		if(lTimer > 30){
+	 			ant.animations.play('run');
+	 		}
+	 	} else {
+	 		//ant.body.velocity.x = 0;
+	 		if(lTimer > 30){
+	 			ant.animations.play('idle');
+	 		}
+	 		ant.body.acceleration.x = -5 * ant.body.velocity.x;
+	 	}
 
- 	if(ant.body.velocity.y!=0){
- 		ant.animations.play('idle');
- 	}
- 	if((upKey.isDown || wKey.isDown) && ant.body.velocity.y ==0){
- 		ant.body.velocity.y = -500;
+	 	if(ant.body.velocity.y!=0){
+	 		ant.animations.play('idle');
+	 	}
+	 	if((upKey.isDown || wKey.isDown) && ant.body.velocity.y ==0){
+	 		ant.body.velocity.y = -500;
+	 	}
  	}
 	///////////////////////////////
 	//Explosion
 	///////////////////////////////
-	if(spaceBar.isDown){
-	    // ant.body.x = 170;
-	    // ant.body.y = 350;
-	    // ant.body.velocity.x = 0;
-	    // ant.body.velocity.y = 0;
+	if(!isDead && deadTimer!=0){
+		deadTimer = 0;
 	}
- 	//Handles input
+	if(isDead){
+		deadTimer++;
+		if(deadTimer > 100){
+			ant.body.velocity.x = -400;
+			ant.body.gravity.y = 0;
+			ant.enableBody = false;		
+		} else {
+			ant.body.velocity.x = 0;
+		}
+	}
+
+	if(spaceBar.isDown){
+	   explode(ant.body.x, ant.body.y);
+	    isDead = true;
+	    ant.animations.stop();
+	   	ant.animations.play('dead');
+		ant.body.gravity.y = 0;
+		ant.enableBody = false;	
+
+	}
 
  	if(backMove==true){
  		//backTrack.body.velocity.x = -ant.body.velocity.x;
  	}
+
+ 	 if(isDead && (ant.body.x < originXPosition)){
+		ant.body.x = originXPosition;
+ 		ant.body.y = -50;
+ 		ant.body.gravity.y = 800;
+ 		isDead = false;
+ 	}
+
 }
 
 function theDeadMethod(){
@@ -293,19 +331,38 @@ function pillCollide(ant, pilC){
 	    pilC.kill();
 	    game.world.setBounds(0,0, game.world.getBounds().width + 820 ,560)
 	    nextColumnXPosition+=720;
-	    generatePillar(nextColumnXPosition, 8);
+	    var cYPos = Math.floor((Math.random() * 9) + 1);
+	    generatePillar(nextColumnXPosition, cYPos);
+	   	generateMorePlatforms(10);
+
+	   	console.log('cYpos: ' + cYPos);
 	}
+}
+
+function generateMorePlatforms(howMany){
+	//THIS NEEDS WORK
+	for(var f = 0; f < howMany; f++){
+		platforms.add(game.add.sprite(nextColumnXPosition + 100 + (Math.random() * 720), (Math.random() * 560), 'platforma1'));
+	}
+	platforms.forEach(function(item){
+	    game.physics.enable([ant, item], Phaser.Physics.ARCADE);
+	    item.body.checkCollision.down = false;
+	    item.body.checkCollision.left = false;
+	    item.body.checkCollision.right = false;
+	    item.body.immovable = true;
+	}, this);
 }
 
 
 function loadLevel1(){
-	var read = new XMLHttpRequest();
-	read.open('GET', 'assets/levels/level1.txt', false);
-	read.send();
-	var platformInfo = read.responseText.split(',');
-	for(i = 0; i < platformInfo.length; i+=2){
-		platforms.add(game.add.sprite(parseInt(platformInfo[i]), parseInt(platformInfo[i+1]), 'platforma1'));	
-	}
+	// var read = new XMLHttpRequest();
+	// read.open('GET', 'assets/levels/level1.txt', false);
+	// read.send();
+	// var platformInfo = read.responseText.split(',');
+	// for(i = 0; i < platformInfo.length; i+=2){
+	// 	platforms.add(game.add.sprite(parseInt(platformInfo[i]), parseInt(platformInfo[i+1]), 'platforma1'));	
+	// }
+	//JOSH I COMMENTED THIS OUT SO I COULD TRY RANDOM PLATFORM GENERATION
 }
 
 
@@ -313,5 +370,4 @@ function onSurface(){
 	onGround = true;
 	lTimer = 0;
 	ant.animations.play('land');
-
 }
